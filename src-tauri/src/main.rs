d21f9be074004b964a2d17f128111febfd8872b6
Jonas Kruckenberg <iterpre@protonmail.com>
2022-12-12 09:16:35 +0100

    feat add logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,36 @@
     windows_subsystem = "windows"
 )]
 
+use env_logger::filter::Builder as FilterBuilder;
+use log::LevelFilter;
+use tauri_plugin_log::{LogTarget, LoggerBuilder};
+#[cfg(debug_assertions)]
+use tauri_plugin_log::fern::colors::ColoredLevelConfig;
+
 fn main() {
+    let log_plugin = {
+        let targets = [
+            LogTarget::LogDir,
+            #[cfg(debug_assertions)]
+            LogTarget::Stdout,
+            #[cfg(debug_assertions)]
+            LogTarget::Webview,
+        ];
+
+        let filter = std::env::var("RUST_LOG")
+            .map(|ref filter| FilterBuilder::new().parse(filter).build().filter())
+            .unwrap_or(LevelFilter::Debug);
+
+        let builder = LoggerBuilder::new().targets(targets).level(filter);
+
+        #[cfg(debug_assertions)]
+        let builder = builder.with_colors(ColoredLevelConfig::default());
+
+        builder.build()
+    };
+
     tauri::Builder::default()
+        .plugin(log_plugin)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
