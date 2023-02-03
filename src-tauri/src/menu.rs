667a5be50949fa3d38f05cec960c441975a330e4
Jonas Kruckenberg <iterpre@protonmail.com>
2023-02-03 15:52:19 +0100

    fmt

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,7 +1,7 @@
-use tauri::{CustomMenuItem, Menu, Submenu, Runtime, WindowMenuEvent, Manager, api::shell::open};
+use tauri::{api::shell::open, CustomMenuItem, Manager, Menu, Runtime, Submenu, WindowMenuEvent};
 
-const ELK_NATIVE_LICENSE : &str= "https://github.com/elk-zone/elk-native/blob/main/LICENSE";
-const ELK_PRIVACY_POLICY_URL : &str= "https://docs.elk.zone/docs/privacy/";
+const ELK_NATIVE_LICENSE: &str = "https://github.com/elk-zone/elk-native/blob/main/LICENSE";
+const ELK_PRIVACY_POLICY_URL: &str = "https://docs.elk.zone/docs/privacy/";
 
 #[cfg(target_os = "windows")]
 pub fn init(app_name: &str) -> Menu {
@@ -33,24 +33,24 @@ pub fn init(app_name: &str) -> Menu {
 
 #[cfg(target_os = "linux")]
 pub fn init(app_name: &str) -> Menu {
-  use tauri::MenuItem;
+    use tauri::MenuItem;
 
-  let file_menu = Menu::new()
-      .add_native_item(MenuItem::CloseWindow)
-      .add_native_item(MenuItem::Quit);
+    let file_menu = Menu::new()
+        .add_native_item(MenuItem::CloseWindow)
+        .add_native_item(MenuItem::Quit);
 
-  let window_menu = Menu::new()
-      .add_native_item(MenuItem::Minimize)
-      .add_native_item(MenuItem::CloseWindow);
+    let window_menu = Menu::new()
+        .add_native_item(MenuItem::Minimize)
+        .add_native_item(MenuItem::CloseWindow);
 
-  let help_menu = Menu::new()
-      .add_item(CustomMenuItem::new("open-license", "View License"))
-      .add_item(CustomMenuItem::new("open-privacy-policy", "Privacy Policy"));
+    let help_menu = Menu::new()
+        .add_item(CustomMenuItem::new("open-license", "View License"))
+        .add_item(CustomMenuItem::new("open-privacy-policy", "Privacy Policy"));
 
-  Menu::new()
-      .add_submenu(Submenu::new("File", file_menu))
-      .add_submenu(Submenu::new("Window", window_menu))
-      .add_submenu(Submenu::new("Help", help_menu))
+    Menu::new()
+        .add_submenu(Submenu::new("File", file_menu))
+        .add_submenu(Submenu::new("Window", window_menu))
+        .add_submenu(Submenu::new("Help", help_menu))
 }
 
 #[cfg(target_os = "macos")]
@@ -60,7 +60,7 @@ pub fn init(app_name: &str) -> Menu {
     let app_menu = Menu::new()
         .add_native_item(MenuItem::About(
             app_name.to_string(),
-            AboutMetadata::default()
+            AboutMetadata::default(),
         ))
         .add_native_item(MenuItem::Separator)
         .add_native_item(MenuItem::Services)
@@ -107,10 +107,10 @@ pub fn handle_menu_event<R: Runtime>(event: WindowMenuEvent<R>) {
     match event.menu_item_id() {
         "open-license" => {
             open(&event.window().shell_scope(), ELK_NATIVE_LICENSE, None).unwrap();
-        },
+        }
         "open-privacy-policy" => {
             open(&event.window().shell_scope(), ELK_PRIVACY_POLICY_URL, None).unwrap();
-        },
+        }
         _ => {}
     }
 }
