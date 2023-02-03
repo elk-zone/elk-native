92323668887a7d5dd16c1a1733f40feb34c7448f
Jonas Kruckenberg <iterpre@protonmail.com>
2023-02-03 21:19:44 +0100

    improve menu (#69)
    * improve menu
    
    * fmt
    
    * fmt

---
+++ .changes/better-menu.md
@@ -0,0 +1,5 @@
+---
+"elk-native": minor
+---
+
+Added menu items to open Privacy Policy and License.
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,10 @@
     windows_subsystem = "windows"
 )]
 
+mod menu;
+
 use env_logger::filter::Builder as FilterBuilder;
 use log::LevelFilter;
-use tauri::Menu;
 #[cfg(debug_assertions)]
 use tauri_plugin_log::fern::colors::ColoredLevelConfig;
 use tauri_plugin_log::{Builder as LogPluginBuilder, LogTarget};
@@ -35,8 +36,13 @@ fn main() {
     tauri::Builder::default()
         .plugin(log_plugin)
         .plugin(StorePluginBuilder::default().build())
-        .plugin(WindowStateBuilder::default().with_state_flags(StateFlags::SIZE | StateFlags::POSITION | StateFlags::MAXIMIZED).build())
-        .menu(Menu::os_default("Elk"))
+        .plugin(
+            WindowStateBuilder::default()
+                .with_state_flags(StateFlags::SIZE | StateFlags::POSITION | StateFlags::MAXIMIZED)
+                .build(),
+        )
+        .menu(menu::init("Elk"))
+        .on_menu_event(menu::handle_menu_event)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
